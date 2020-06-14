db304b88a091473839cec2a9e91c650990140093
mashort <ma.short@yahoo.co.uk>
2020-06-14 18:53:15 +0100

    Add Munros controller with stub GET endpoint

---
+++ Munros.API/Controllers/MunrosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Munros.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MunrosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMunros()
+        {
+            return Ok("OK.");
+        }
+    }
+}
