46dc297dde955637467dd7fcdb39508e1da94e09
mashort <ma.short@yahoo.co.uk>
2020-06-14 18:35:59 +0100

    Add support for Entity Framework Core, including In-Memory database

--- Munros.API/Startup.cs
+++ Munros.API/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Munros.Infrastructure.Data;
 
 namespace Munros.API
 {
@@ -18,6 +20,8 @@ namespace Munros.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseInMemoryDatabase("Munros"));
             services.AddControllers();
         }
 
---
+++ Munros.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Munros.Core.Entities;
+
+namespace Munros.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Munro> Munros { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
