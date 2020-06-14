5c862ed8b14d82ef47675fef31bc0654f1da31b0
mashort <ma.short@yahoo.co.uk>
2020-06-14 18:24:03 +0100

    Add data model

---
+++ Munros.Core/Entities/Munro.cs
@@ -0,0 +1,12 @@
+﻿namespace Munros.Core.Entities
+{
+    public class Munro
+    {
+        //[JsonIgnore]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Height { get; set; }
+        public string Category { get; set; }
+        public string GridReference { get; set; }
+    }
+}
